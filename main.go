ca1a8e8b6edc98bbd1c14fddc7e5a72406181067
gfxop <gfxop@outlook.com>
2019-03-24 20:53:57 -0500

    run server

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello W2orld")
+}
+
 func main() {
-	fmt.Println("Hello wold")
+	http.HandleFunc("/", helloWorld)
+	http.ListenAndServe(":8080", nil)
 }
